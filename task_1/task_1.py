1a9ccf822239ef4b7d9b64f136029c3bd7038a6b
Денис Уцеха <d.utsekha@reactor.com.ua>
2024-04-28 13:41:26 +0300

    first commit

---
+++ task_1/task_1.py
@@ -0,0 +1,40 @@
+import logging
+import argparse
+import asyncio
+
+from aioshutil import copyfile
+from aiopath import AsyncPath
+
+parser = argparse.ArgumentParser(description='Sorting folder')
+parser.add_argument('--source', "-s", help='Source folder', required=True)
+parser.add_argument('--output', "-o", help='Output folder', default="dist")
+
+args = vars(parser.parse_args())
+
+source = AsyncPath(args.get("source"))
+output = AsyncPath(args.get("output"))
+
+
+async def read_folder(path):
+    async for el in path.iterdir():
+        if await el.is_dir():
+            await read_folder(el)
+        else:
+            await copy_file(el)
+
+
+async def copy_file(file):
+    ext_folder = output / file.suffix[1:]
+    try:
+        await ext_folder.mkdir(exist_ok=True, parents=True)
+        await copyfile(file, ext_folder / file.name)
+    except OSError as err:
+        logging.error(err)
+
+
+if __name__ == "__main__":
+    message_format = "%(threadName)s %(asctime)s: %(message)s"
+    logging.basicConfig(level=logging.INFO,
+                        format=message_format, datefmt="%H:%M:%S")
+    asyncio.run(read_folder(source))
+    print(f"Copying is done!")
